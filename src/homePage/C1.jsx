ddea53d9799113c7ccf0e41d2c75ea8b1673a8d6
divyansh-1214 <divyansh.sri23@gmail.com>
2024-12-25 01:20:55 +0530

    1 done

--- src/homePage/C1.jsx
+++ src/homePage/C1.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Card from './Card';
 const C1 = () => {
     return (
-        <div className='cantainer bg-gray-600 '>
-            <h1>Get started with ELEAT</h1>
-            <p>All-in-one protein breakfast to help you perform your best</p>
-            <div className='card-container '>
+        <div className='cantainer bg-gray-600 h-full' style={{background:'#f2f0ed'}}>
+            <div className='flex flex-col items-center justify-center pt-16' >
+            <h1 className='text-5xl pb-6'>Get started with ELEAT</h1>
+            <p className='text-base font-semibold'>All-in-one protein breakfast to help you perform your best</p>
+            </div>
+            <div className='card-container grid grid-cols-4 gap-0 p-16'  >
+                <Card className=' items-center justify-center' title='PROTEIN CEREAL' description='250g pouches - 5 x 50g servings' imageUrl='./public/card11.png' />
                 <Card title='PROTEIN CEREAL' description='250g pouches - 5 x 50g servings' imageUrl='./public/card12.png' />
-                <Card title='PROTEIN CEREAL' description='250g pouches - 5 x 50g servings' imageUrl='./1.png' />
-                <Card title='PROTEIN CEREAL' description='250g pouches - 5 x 50g servings' imageUrl='./1.png' />
-                <Card title='PROTEIN CEREAL' description='250g pouches - 5 x 50g servings' imageUrl='./1.png' />
+                <Card title='PROTEIN CEREAL' description='250g pouches - 5 x 50g servings' imageUrl='./public/card13.png' />
+                <Card title='PROTEIN CEREAL' description='250g pouches - 5 x 50g servings' imageUrl='./public/card14.png' />
             </div>
         </div>
     );
--- src/homePage/Card.jsx
+++ src/homePage/Card.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Card = ({ title, description, imageUrl }) => {
     return (
-        <div className="card">
-            <img src={imageUrl} alt={title} className="card-image w-72" />
-            <div className="card-content bg-neutral-300 w-32">
+        <div className="card w-72 h-auto rounded-3xl">
+            <img  src={imageUrl} alt={title} className="card-image rounded-t-3xl w-72" />
+            <div className="w-fit-content flex flex-col justify-center  items-center p-7">
                 <h2 className="card-title">{title}</h2>
                 <p className="card-description">{description}</p>
+                <button className='w-fit flex justify-center p-1 mt-2 bg-black text-fuchsia-50'>SHOP NOW</button>
             </div>
         </div>
     );
 };
 
 export default Card;
+
